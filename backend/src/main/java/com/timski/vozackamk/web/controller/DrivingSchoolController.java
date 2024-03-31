19744ea829e3cf4d85567979111a6ee7f0f481de
TeaK16 <teakrstevska01@gmail.com>
2024-03-31 18:58:07 +0200

    cross origin and docker compose update

--- backend/src/main/java/com/timski/vozackamk/web/controller/DrivingSchoolController.java
+++ backend/src/main/java/com/timski/vozackamk/web/controller/DrivingSchoolController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/drivingschools")
 public class DrivingSchoolController {
     private final DrivingSchoolService drivingSchoolService;
