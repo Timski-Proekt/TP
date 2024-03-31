0a737b4b9d061eca4ce0a815a9294ca47019ac30
TeaK16 <teakrstevska01@gmail.com>
2024-03-31 18:47:09 +0200

    Update

--- backend/src/main/java/com/timski/vozackamk/web/controller/DrivingSchoolController.java
+++ backend/src/main/java/com/timski/vozackamk/web/controller/DrivingSchoolController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/drivingschools")
 public class DrivingSchoolController {
     private final DrivingSchoolService drivingSchoolService;
